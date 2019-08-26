f3aa858e6b0ea9feb17fabb7b1f0ee85d9a83ee2
LuchoPonce <luis.ponce@utec.edu.pe>
2019-08-25 17:07:55 -0700

    Final Commit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,34 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
 
 using namespace std;
 
 int main() {
+
+	string palabra,modificado;
+	vector<string>imprimir;
+	unordered_map<string,pair<string,int>> lista;	
+	std::vector<string>::iterator it;
+	while(cin>>palabra){
+		if(palabra=="#"){break;}
+		modificado = palabra;
+		transform(modificado.begin(),modificado.end(),modificado.begin(), ::tolower);
+		sort(modificado.begin(),modificado.end());
+		lista[modificado].second++;
+		lista[modificado].first = palabra;
+
+	}
+	for(auto val : lista){
+		if(val.second.second==1){imprimir.push_back(val.second.first);}
+	}
+
+	sort(imprimir.begin(),imprimir.end());
+
+	for(auto val : imprimir){
+			cout<<val<<endl;
+		}
+
+
 	return EXIT_SUCCESS;
 }
 
